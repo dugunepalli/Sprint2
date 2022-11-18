81d6fb21251a527f466472ebf57ff54576549e63
dugunepalli <116146270+dugunepalli@users.noreply.github.com>
2022-11-18 10:05:01 +0530

    Create Logger.h

---
+++ Logger.h
@@ -0,0 +1,89 @@
+
+#include<stdio.h>
+
+
+enum LogPriority
+{
+    TracePriority, DebugPriority, InfoPriority, WarnPriority, 
+    ErrorPriority, CriticalPriority
+};
+class Logger()
+{
+    private:
+        static LogPriority priority;
+    public:
+        static void set_priority(LogPriority new_priority)
+        {
+            priority=new_priority;
+        }
+        
+        templete<typename... Args>
+        static void Trace(const char* message, Args... args)
+        {
+            if(priority<=TracePriority)
+            {
+                printf("[Trace]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+        
+        templete<typename... Args>
+        static void Debug(const char* message, Args... args)
+        {
+            if(priority<=DebugPriority)
+            {
+                printf("[Debug]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+        templete<typename... Args>
+        static void Info(const char* message, Args... args)
+        {
+            if(priority<=InfoPriority)
+            {
+                printf("[Info]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+        templete<typename... Args>
+        static void Warn(const char* message, Args... args)
+        {
+            if(priority<=WarnPriority)
+            {
+                printf("[Warn]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+        templete<typename... Args>
+        static void Error(const char* message, Args... args)
+        {
+            if(priority<=ErrorPriority)
+            {
+                printf("[Error]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+        templete<typename... Args>
+        static void Critical(const char* message, Args... args)
+        {
+            if(priority<=CriticalPriority)
+            {
+                printf("[Critical]\t");
+                printf(message, args...);
+                printf("\n");
+            }
+        }
+        
+};
+
+LogPriority Logger::priority=InfoPriority;
